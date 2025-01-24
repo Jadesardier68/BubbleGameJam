404c1e0ce416686a19b80ae8dc608ff40204cdc4
jurkau <hugoweber@orange.fr>
2025-01-24 10:37:02 +0100

    reparation mouvement personnage sur plateforme

--- BubbleJam/Assets/Codes/RotationWheelController.cs
+++ BubbleJam/Assets/Codes/RotationWheelController.cs
@@ -14,4 +14,14 @@ public class RotationWheelController : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, 0);
         transform.RotateAround(baseObject.position, Vector3.back, rotationSpeed * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
 }
